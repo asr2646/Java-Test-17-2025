9ba3cf872b8b7c366143f3054bea972ac5bf6b70
Abhishek <abhishekrajput301999@gmail.com>
2025-11-03 23:46:33 +0530

    Print "Java" 5 times using while loop

---
+++ WhileLoop5times.java
@@ -0,0 +1,13 @@
+public class WhileLoop5times {
+    public static void main(String[] args) {
+
+        int i = 1;  // initialization
+
+        System.out.println("Printing 'Java' 5 times:");
+
+        while (i <= 5) {  // condition
+            System.out.println("Java");
+            i++;  // increment
+        }
+    }
+}
