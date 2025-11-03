8ce0ef77c00fc50fcc3d5ccf7e70007e144575b9
Abhishek <abhishekrajput301999@gmail.com>
2025-11-03 23:32:31 +0530

    Print the sum of first 10 natural numbersusing for Loop

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            sum = sum + i;  // or sum += i;
+        }
+
+        System.out.println("Sum of first 10 natural numbers is: " + sum);
+    }
+}
