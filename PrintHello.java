dc24107bf686d6c91da997fe8e18fd3eceb8edb2
Abhishek <abhishekrajput301999@gmail.com>
2025-11-03 23:30:34 +0530

    Print "Hello" 5 times using for Loop

---
+++ PrintHello.java
@@ -0,0 +1,10 @@
+public class PrintHello {
+    public static void main(String[] args) {
+
+        System.out.println("Printing 'Hello' 5 times:");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
