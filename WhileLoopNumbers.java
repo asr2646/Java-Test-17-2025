c98d52a9fd4bf688e433dfccb7ba059b132f69db
Abhishek <abhishekrajput301999@gmail.com>
2025-11-03 23:38:41 +0530

    Print numbers from 1 to 10 while loop

---
+++ WhileLoopNumbers.java
@@ -0,0 +1,13 @@
+public class WhileLoopNumbers {
+    public static void main(String[] args) {
+
+        int i = 1;  // initialization
+
+        System.out.println("Numbers from 1 to 10:");
+
+        while (i <= 10) {  // condition
+            System.out.println(i);
+            i++;  // increment
+        }
+    }
+}
