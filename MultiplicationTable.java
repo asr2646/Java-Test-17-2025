043579b07804030129e4b1418a1c2725c335887a
Abhishek <abhishekrajput301999@gmail.com>
2025-11-03 23:35:26 +0530

    Print the multiplication table of 5 using for Loop

---
+++ MultiplicationTable.java
@@ -0,0 +1,10 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        System.out.println("Multiplication Table of 5:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+    }
+}
