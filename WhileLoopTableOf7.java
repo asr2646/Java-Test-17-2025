d1786ee79397d30310af9695cdc478a086bd817a
Abhishek <abhishekrajput301999@gmail.com>
2025-11-03 23:51:42 +0530

    Print the multiplication table of 7 using while loop

---
+++ WhileLoopTableOf7.java
@@ -0,0 +1,13 @@
+public class WhileLoopTableOf7 {
+    public static void main(String[] args) {
+
+        int i = 1;  // initialization
+
+        System.out.println("Multiplication Table of 7:");
+
+        while (i <= 10) {  // condition
+            System.out.println("7 x " + i + " = " + (7 * i));
+            i++;  // increment
+        }
+    }
+}
